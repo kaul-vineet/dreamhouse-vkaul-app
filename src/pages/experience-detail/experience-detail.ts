ba4afc0e4e605818f666443afb0a25efcbaec964
unknown <vkaul@salesforce.com>
2020-06-28 00:43:19 +0530

    ïntegrate

--- src/pages/experience-detail/experience-detail.ts
+++ src/pages/experience-detail/experience-detail.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser"
 
