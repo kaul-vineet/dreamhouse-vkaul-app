326c9469a3cfcbb7cec31b6688e0e240008f1466
unknown <vkaul@salesforce.com>
2020-06-27 23:40:58 +0530

    ïntegrate

---
+++ src/pages/experience-detail/experience-detail.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-experience-detail',
+    templateUrl: 'experience-detail.html'
+})
+export class ExperienceDetailPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
