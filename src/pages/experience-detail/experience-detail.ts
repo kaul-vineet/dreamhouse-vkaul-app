97b8ea2d5b98208e6650ec32b6f66d6f4a9de23b
unknown <vkaul@salesforce.com>
2020-06-28 00:37:33 +0530

    ïntegrate

--- src/pages/experience-detail/experience-detail.ts
+++ src/pages/experience-detail/experience-detail.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser"
+
 
 @Component({
     selector: 'page-experience-detail',
     templateUrl: 'experience-detail.html'
 })
-export class ExperienceDetailPage {
 
-  constructor(public navCtrl: NavController) {
+export class ExperienceDetailPage implements OnInit {
+  vidUrl:SafeResourceUrl;
+  constructor(private domSatizer: DomSanitizer) {
 
+  }
+	
+  ngOnInit() {
+  this.vidUrl = this.domSatizer.bypassSecurityTrustResourceUrl("https://vrar-millenial-falcon.herokuapp.com/");
   }
 
 }
